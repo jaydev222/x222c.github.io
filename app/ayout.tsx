23e0ab743a811c4ac9c0121bc36057e6844b7c58
jaydev222 <giorgiorosarosso02@gmail.com>
2024-12-06 20:05:27 +0000

    first implementation

---
+++ app/ayout.tsx
@@ -0,0 +1,22 @@
+import "./globals.css"
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ["latin"] })
+
+export const metadata = {
+  title: "x222c.eu - Crafting Digital Experiences",
+  description: "We specialize in web design, branding, and digital marketing to drive results for your business.",
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
+
