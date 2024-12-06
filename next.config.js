bf1128d3cdee44db17026009617b487148a193f2
jaydev222 <giorgiorosarosso02@gmail.com>
2024-12-06 22:18:24 +0000

    new version

--- next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  basePath: '/x222c.github.io', // Replace with your repo name
-  trailingSlash: true,
-};
-
-module.exports = nextConfig;
--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite";
-
-export default defineConfig({
-  base: "/x222c.github.io/", // Replace with your repo name if different
-  build: {
-    outDir: "dist",
-  },
-});
